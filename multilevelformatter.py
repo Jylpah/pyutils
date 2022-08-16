5b94f08f157091b7c104beda4a270aa6d46557cd
jylpah <jylpah@gmail.com>
2022-08-16 22:09:26 +0300

    mypy: OK

--- multilevelformatter.py
+++ multilevelformatter.py
@@ -1,18 +1,18 @@
 import logging
-from typing import Optional
+from typing import Literal, Optional
 
 class MultilevelFormatter(logging.Formatter):
 		
 	_levels: list[int] = [logging.NOTSET, logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]
 
-	def __init__(self, fmts: dict[int, str], fmt=None, datefmt=None, style='%', validate=True, *, defaults=None):
+	def __init__(self, fmts: dict[int, str], fmt: Optional[str]=None, datefmt: Optional[str]=None, 
+					style:Literal['%', '{', '$'] ='%', validate: bool=True, *, defaults = None):
 		assert fmts is not None, "styles cannot be None"
-		# to avoid hassle... 
-	
-		default_formatters: dict[int, logging.Formatter] = dict()
+		
+		self._formatters: dict[int, logging.Formatter] = dict()
 		for level in self._levels:
-			default_formatters[level] = logging.Formatter(fmt=fmt, datefmt=datefmt, style=style, validate=validate, defaults=defaults)
-		self._formatters : dict[int, logging.Formatter] = default_formatters
+			self._formatters[level] = logging.Formatter(fmt=fmt, datefmt=datefmt, style=style, validate=validate, defaults=defaults)
+		
 		for level in fmts.keys():
 			self._formatters[level] = logging.Formatter(fmt=fmts[level], style=style)
 		
