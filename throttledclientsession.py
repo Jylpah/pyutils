1c7521a0d44b8243c579f3ab83e1aeda1a5084e6
jylpah <jylpah@gmail.com>
2022-09-21 23:56:22 +0300

    Support for low rates

--- throttledclientsession.py
+++ throttledclientsession.py
@@ -14,26 +14,21 @@ import logging
 
 class ThrottledClientSession(aiohttp.ClientSession):
     """Rate-throttled client session class inherited from aiohttp.ClientSession)""" 
-    MIN_SLEEP = 0.001
 
     def __init__(self, rate_limit: float = 0, *args,**kwargs) -> None: 
         super().__init__(*args,**kwargs)
-        assert rate_limit is not None and rate_limit >= 0, "rate_limit is None or below zero"
-
-        self.rate_limit: float = rate_limit
-        self._fillerTask = None
-        self._queue = None
+        
+        self.rate_limit: float
+        self._fillerTask    : Optional[asyncio.Task]= None
+        self._queue         : Optional[asyncio.Queue]               = None
         self._start_time = time.time()
         self._count = 0   
-
-        if rate_limit > 0:
-            self._queue = asyncio.Queue(min(2, int(rate_limit)+1))            
-            self._fillerTask = asyncio.create_task(self._filler())
+        self.set_rate_limit(rate_limit)
 
 
     def _get_sleep(self) -> float:        
         if self.rate_limit > 0:
-            return max(1/self.rate_limit, self.MIN_SLEEP)
+            return 1/self.rate_limit
         return 0
 
 
@@ -66,6 +61,12 @@ class ThrottledClientSession(aiohttp.ClientSession):
         assert rate_limit is not None, "rate_limit must not be None" 
         assert isinstance(rate_limit, float) and rate_limit >= 0, "rate_limit has to be type of 'float' >= 0"
         self.rate_limit = rate_limit
+
+        if rate_limit > 0:
+            self._queue     = asyncio.Queue(int(rate_limit)+1) 
+            if self._fillerTask is not None: 
+                self._fillerTask.cancel()  
+            self._fillerTask = asyncio.create_task(self._filler())
         return self.rate_limit
         
 
@@ -73,28 +74,26 @@ class ThrottledClientSession(aiohttp.ClientSession):
         """Close rate-limiter's "bucket filler" task"""
         # DEBUG 
         logging.debug(self.get_stats_str())
-        if self._fillerTask != None:
+        if self._fillerTask is not None:
             self._fillerTask.cancel()
         try:
-            await asyncio.wait_for(self._fillerTask, timeout= 0.5)
+            await asyncio.wait_for(self._fillerTask, timeout=0.5)
         except asyncio.TimeoutError as err:
             logging.error(str(err))
         await super().close()
 
-
-    # async def _filler(self, rate_limit: float = 1):
+    
     async def _filler(self) -> None:
         """Filler task to fill the leaky bucket algo"""
         try:
             if self._queue is None:
-                return None
-            sleep = self._get_sleep()
-            logging.debug('SLEEP: ' + str(sleep))
+                return None            
+            logging.debug('SLEEP: ' + str(self._get_sleep()))
             updated_at = time.monotonic()
-            fraction = 0
-            extra_increment = 0
+            fraction        : float = 0
+            extra_increment : float = 0
             for i in range(0, self._queue.maxsize):
-                self._queue.put_nowait(i)
+                await self._queue.put(i)
             while True:
                 if not self._queue.full():
                     now = time.monotonic()
@@ -106,8 +105,7 @@ class ThrottledClientSession(aiohttp.ClientSession):
                     for i in range(0,items_2_add):
                         self._queue.put_nowait(i)
                     updated_at = now
-                if sleep > 0:
-                    await asyncio.sleep(sleep)
+                await asyncio.sleep(self._get_sleep())
         except asyncio.CancelledError:
             logging.debug('Cancelled')
         except Exception as err:
