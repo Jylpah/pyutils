7a072eaeb8dd2f25c30f085fa602abbd5075c889
Jylpah <jylpah@gmail.com>
2022-11-21 19:34:50 +0200

    CounterQueue(): count_items flag

--- counterqueue.py
+++ counterqueue.py
@@ -16,21 +16,32 @@ debug	= logger.debug
 T = TypeVar('T')
 
 class CounterQueue(Queue[T]):
-	_counter : int
+	_counter 	 : int
+	_count_items : bool
 
-	def __init__(self, *args, **kwargs) -> None:
+	def __init__(self, *args, count_items : bool = True, **kwargs) -> None:
 		super().__init__(*args, **kwargs)
 		self._counter = 0
+		self._count_items = count_items
 
 	def task_done(self) -> None:
 		super().task_done()
-		self._counter += 1
+		if self._count_items:
+			self._counter += 1
 		return None
+
 
 	@property
 	def count(self) -> int:
 		"""Return number of completed tasks"""
 		return self._counter
+
+
+	@property
+	def count_items(self) -> bool:
+		"""Whether or not count items"""
+		return self._count_items
+
 
 async def alive_queue_bar(queues : Iterable[CounterQueue], title : str, 
 							total : int | None = None, wait: float = 0.1, 
