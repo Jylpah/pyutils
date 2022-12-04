c6923d7f97aba6186d087dc50f83a1be0fe5b01c
Jylpah <jylpah@gmail.com>
2022-12-05 00:17:02 +0200

    setLevels()

--- multilevelformatter.pyi
+++ multilevelformatter.pyi
@@ -15,6 +15,16 @@ class MultilevelFormatter(logging.Formatter):
 	def __init__(self, fmts: dict[int, str], fmt: Optional[str]=None, datefmt: Optional[str]=None, 
 					style:Literal['%', '{', '$'] ='%', validate: bool=True, *, defaults = None): ...
 
+	@classmethod
+	def setLevels(cls, 
+					logger: 	logging.Logger, 
+					fmts: 		Optional[dict[int, str]]	= None, 
+					fmt: 		Optional[str] 				= None, 
+					datefmt: 	Optional[str]				= None,
+					style:		Literal['%', '{', '$']		= '%', 
+					validate:	bool						= True, 
+					log_file: 	Optional[str] 				= None) -> None: ...
+
 	def format(self, record: logging.LogRecord) -> str: ...
 
 	def formatTime(self, record: logging.LogRecord, datefmt: Optional[str]=None): ...
