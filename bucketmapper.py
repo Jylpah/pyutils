7f5c1e4fb9a346d3dc32c7df041694cdb48a122a
Jylpah <jylpah@gmail.com>
2022-12-07 18:15:44 +0200

    tested and works

--- bucketmapper.py
+++ bucketmapper.py
@@ -1,6 +1,5 @@
-from collections import namedtuple
 from operator import attrgetter
-from bisect import bisect, insort
+from bisect import bisect, insort, bisect_left
 from typing import Any, TypeVar, Generic, Optional
 from dataclasses import dataclass
 import logging
@@ -14,25 +13,10 @@ debug	= logger.debug
 
 T = TypeVar("T")
 
-# @dataclass
-# class BucketItem(Generic[T]):
-# 	item: T
-# 	key: int | float
-
-# class BucketMapper(Generic[T]):
-	
-# 	by_key = attrgetter('key')
-	
-# 	def __init__(self, attr: str):
-# 		self._attr : str = attr
-# 		#self.keys : list[float|int] = list()
-# 		self.data : list[BucketItem[T]] = list()
-
-# 	def insert(self, item : T) -> None:
-# 		key : int | float = getattr(item, self._attr)
-# 		insort(self.data, BucketItem(item=item, key=key), key=self.by_key)
-# 		return None
-		
+@dataclass
+class BucketItem(Generic[T]):
+	item: T
+	key: int | float
 
 class BucketMapper(Generic[T]):
 	
@@ -40,10 +24,12 @@ class BucketMapper(Generic[T]):
 		self.by_key = attrgetter(attr)
 		self.data : list[T] = list()
 
+
 	def insert(self, item : T) -> None:
 		insort(self.data, item, key=self.by_key)
 		return None
 	
+
 	def get(self, key: int | float) -> Optional[T]:
 		try:
 			return self.data[bisect(self.data, key, key=self.by_key)]
@@ -51,9 +37,15 @@ class BucketMapper(Generic[T]):
 			error(f'{err}')
 		return None
 
+
 	def pop(self, item : T) -> Optional[T]:
 		try:
-			return self.data.pop(bisect(self.data, self.by_key(item), key=self.by_key))
+			return self.data.pop(bisect_left(self.data, self.by_key(item), key=self.by_key))
 		except Exception as err:
 			error(f'{err}')
 		return None
+	
+
+	def list(self) -> list[T]:
+		return self.data
+		
