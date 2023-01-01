ffe34244a0e33a9e7ca3387926a59799d41a950a
Jylpah <jylpah@gmail.com>
2023-01-01 02:19:06 +0200

    shutdown()

--- iterablequeue.py
+++ iterablequeue.py
@@ -1,17 +1,22 @@
 from asyncio import Queue
-from typing import Any, AsyncIterable
+from typing import Any, AsyncIterable, Generic, TypeVar, Optional
+from .utils import Countable
 
+
+T= TypeVar('T')
 
 class QueueDone(Exception):
 	pass
 
-
-class IterableQueue(Queue, AsyncIterable):
+class IterableQueue(Queue[Optional[T]], AsyncIterable, Countable):
 	"""Async.Queue subclass to support async iteration and QueueDone"""
-	def __init__(self, producers: int = 1, **kwargs):
-		super(Queue,self).__init__(**kwargs)
+
+	def __init__(self, producers: int = 1, total: int = 0, **kwargs):
+		super(Queue[Optional[T]],self).__init__(**kwargs)
 		self._producers = producers
 		self._done : bool = False
+		self._total : int = total
+		self._count : int = 0
 		self._registered_producers : bool = producers > 1
 
 
@@ -32,12 +37,17 @@ class IterableQueue(Queue, AsyncIterable):
 		return self._done
 
 
+	async def shutdown(self) -> None:
+		self._producers = 0
+		await self.done()
+
+
 	@property
 	def is_done(self) -> bool:
 		return self._done
 
 
-	async def put(self, item) -> None:
+	async def put(self, item: Optional[T]) -> None:
 		if self._done:
 			raise QueueDone
 		else:
@@ -46,21 +56,27 @@ class IterableQueue(Queue, AsyncIterable):
 			await super().put(item=item)
 
 
-	async def get(self) -> Any:
+	async def get(self) -> T:
 		item = await super().get()
 		if item is None:
 			super().task_done()
+			self._done = True
 			await super().put(None)
 			raise QueueDone
 		else:
+			self._count += 1
 			return item
 
-	
+	@property
+	def count(self) -> int:
+		return min(self._count, self._total)
+
+
 	async def __aiter__(self):
 		return self
 
 	
-	async def __anext__(self) -> Any:
+	async def __anext__(self) -> T:
 		try:
 			item = await self.get()
 			return item
