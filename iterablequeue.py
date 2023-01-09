2274124d3004c0c419a50441d74ba6eefd0b1d95
Jylpah <jylpah@gmail.com>
2023-01-09 17:36:16 +0200

    BROKEN: trying to use Queue[T]

--- iterablequeue.py
+++ iterablequeue.py
@@ -8,20 +8,25 @@ T= TypeVar('T')
 class QueueDone(Exception):
 	pass
 
-class IterableQueue(Queue[Optional[T]], AsyncIterable, Countable):
+
+class IterableQueue(Queue[T], AsyncIterable, Countable):
 	"""Async.Queue subclass to support async iteration and QueueDone"""
 
-	def __init__(self, producers: int = 1, total: int = 0, **kwargs):
-		super(Queue[Optional[T]],self).__init__(**kwargs)
-		self._producers = producers
-		self._done : bool = False
-		self._total : int = total
-		self._count : int = 0
+	def __init__(self, producers: int = 1, total: int = 0, 
+					count_items: bool = True, **kwargs):
+		super(Queue[Optional[T]], self).__init__(**kwargs)
+		self._producers 	: int 	= producers
+		self._done 			: bool 	= False
+		self._total 		: int 	= total
+		self._count 		: int 	= 0
+		self._count_items 	: bool 	= count_items
 		self._registered_producers : bool = producers > 1
 
 
 	def register_producer(self, N : int = 1) -> int:
 		assert N > 0, 'N has to be positive'
+		if self.is_done:
+			raise ValueError('Cannot register a producer to a queue that is done')
 		if self._registered_producers:
 			self._producers += N
 		else:
@@ -32,6 +37,7 @@ class IterableQueue(Queue[Optional[T]], AsyncIterable, Countable):
 	async def done(self) -> bool:
 		self._producers -= 1
 		if self._producers <= 0:
+			self._producers = 0
 			self._done = True
 			await super().put(None)
 		return self._done
@@ -47,29 +53,36 @@ class IterableQueue(Queue[Optional[T]], AsyncIterable, Countable):
 		return self._done
 
 
-	async def put(self, item: Optional[T]) -> None:
-		if self._done:
+	async def put(self, item: T) -> None:
+		if self.is_done:
 			raise QueueDone
 		else:
 			if item is None:
 				raise ValueError('Cannot add None to IterableQueue')
 			await super().put(item=item)
 
+	
+	# async def last(self) -> None:
+	# 	"""Put marker to the queue signaling the last item has 
+	# 		been put to the queue"""
+	# 	await super().put(None)
+
 
 	async def get(self) -> T:
 		item = await super().get()
 		if item is None:
 			super().task_done()
-			self._done = True
-			await super().put(None)
+			await super().put(None)			
 			raise QueueDone
 		else:
-			self._count += 1
+			if self._count_items:
+				self._count += 1
 			return item
 
 	@property
 	def count(self) -> int:
-		return min(self._count, self._total)
+		# return min(self._count, self._total)
+		return self._count
 
 
 	async def __aiter__(self):
@@ -82,4 +95,3 @@ class IterableQueue(Queue[Optional[T]], AsyncIterable, Countable):
 			return item
 		except QueueDone:
 			raise StopAsyncIteration
-
