8f07a98e5c82a66d0f7a23698327b3facfb0921d
Jylpah <jylpah@gmail.com>
2023-01-22 21:39:17 +0200

    AsyncQueue(): Async wrapper for queue.Queue()

---
+++ asyncqueue.py
@@ -0,0 +1,57 @@
+from queue import Queue, Full, Empty
+from typing import Any, AsyncIterable, Generic, TypeVar, Optional
+import logging 
+from asyncio import sleep
+
+T= TypeVar('T')
+
+logger = logging.getLogger(__name__)
+
+debug	= logger.debug
+message = logger.warning
+verbose = logger.info
+error 	= logger.error
+
+class AsyncQueue(Generic[T]):
+	"""Async wrapper for queue.Queue"""
+
+	SLEEP: float = 0.01
+
+	def __init__(self, queue: Queue[T]):
+		self._Q : Queue[T] = queue
+
+	async def get(self) -> T:
+		while True:
+			try:
+				return self._Q.get_nowait()
+			except Empty:
+				await sleep(self.SLEEP)
+	
+
+	async def put(self, item: T) -> None:
+		
+		while True:
+			try:
+				self._Q.put_nowait(item)
+				return None
+			except Full:
+				await sleep(self.SLEEP)
+
+
+	def task_done(self) -> None:
+		self._Q.task_done()
+		return None
+
+
+	def qsize(self) -> int:
+		return self._Q.qsize()
+
+
+	def empty(self) -> bool:
+		return self._Q.empty()
+
+
+	def full(self) -> bool:
+		return self._Q.full()
+
+
