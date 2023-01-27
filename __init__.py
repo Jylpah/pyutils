b857dd7d5990e5c3d357066411451dbdffc6afff
Jylpah <jylpah@gmail.com>
2023-01-27 16:38:21 +0200

    Add BackendIndex TypeVars

--- __init__.py
+++ __init__.py
@@ -10,7 +10,8 @@ from .throttledclientsession import ThrottledClientSession
 from .urlqueue 				import UrlQueue, UrlQueueItemType, is_url
 from .utils					import TXTExportable, CSVExportable, JSONExportable, \
 									CSVImportable, JSONImportable, TXTImportable, \
-									Countable, I, D, O, Idx, \
+									Countable, I, D, O, Idx, BackendIndexType, BackendIndex, \
+									DESCENDING, ASCENDING, TEXT, \
 									export, epoch_now, alive_bar_monitor, is_alphanum, \
 									get_url, get_urls, \
 									get_url_JSON_model, get_url_JSON, get_urls_JSON, \
