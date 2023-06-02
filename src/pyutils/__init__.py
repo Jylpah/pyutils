0048008e185b193c190983da87a07cfe7851d23f
jylpah <jylpah@gmail.com>
2023-06-02 23:48:30 +0300

    AsyncWrapIterable(AsyncIterable[T])

--- src/pyutils/__init__.py
+++ src/pyutils/__init__.py
@@ -1,52 +1,70 @@
-from .aliasmapper 			import AliasMapper as AliasMapper
-from .asyncqueue 			import AsyncQueue as AsyncQueue  
-from .bucketmapper			import BucketMapper as BucketMapper
-from .counterqueue 			import CounterQueue as CounterQueue, \
-								 	QCounter as QCounter
-from .eventcounter 			import EventCounter as EventCounter
-from .filequeue 			import FileQueue as FileQueue
-from .iterablequeue 		import IterableQueue as IterableQueue, QueueDone as QueueDone
-from .multilevelformatter 	import MultilevelFormatter as MultilevelFormatter
+from .aliasmapper import AliasMapper as AliasMapper
+from .asyncqueue import AsyncQueue as AsyncQueue
+from .awrap import AsyncWrapIterable as AsyncWrapIterable
+from .bucketmapper import BucketMapper as BucketMapper
+from .counterqueue import CounterQueue as CounterQueue, QCounter as QCounter
+from .eventcounter import EventCounter as EventCounter
+from .filequeue import FileQueue as FileQueue
+from .iterablequeue import IterableQueue as IterableQueue, QueueDone as QueueDone
+from .multilevelformatter import MultilevelFormatter as MultilevelFormatter
 from .throttledclientsession import ThrottledClientSession as ThrottledClientSession
-from .urlqueue 				import UrlQueue as UrlQueue, UrlQueueItemType, is_url
-from .exportable 			import TXTExportable as TXTExportable, \
-									CSVExportable as CSVExportable, \
-									JSONExportable as JSONExportable, \
-									TypeExcludeDict as TypeExcludeDict, \
-									BackendIndexType as BackendIndexType, \
-									BackendIndex as BackendIndex, \
-									Idx as Idx, \
-									export as export,\
-									export_csv as export_csv, \
-									export_json as export_json, \
-									export_txt as export_txt, \
-									DESCENDING as DESCENDING, \
-									ASCENDING as ASCENDING, \
-									TEXT as TEXT, \
-									I as I, \
-									D as D, \
-									O as O
-from .importable 			import CSVImportable as CSVImportable, \
-									JSONImportable as JSONImportable,\
-	 								TXTImportable as TXTImportable, \
-									Importable as Importable, \
-									CSVImportableSelf, JSONImportableSelf, TXTImportableSelf
-from .utils					import Countable as Countable,\
-									epoch_now, alive_bar_monitor, is_alphanum, \
-									get_url, get_urls, get_url_JSON_model, get_url_JSON_models,\
-									get_url_JSON, get_urls_JSON, get_urls_JSON_models,\
-									get_datestr, chunker
+from .urlqueue import UrlQueue as UrlQueue, UrlQueueItemType, is_url
+from .exportable import (
+    TXTExportable as TXTExportable,
+    CSVExportable as CSVExportable,
+    JSONExportable as JSONExportable,
+    TypeExcludeDict as TypeExcludeDict,
+    BackendIndexType as BackendIndexType,
+    BackendIndex as BackendIndex,
+    Idx as Idx,
+    export as export,
+    export_csv as export_csv,
+    export_json as export_json,
+    export_txt as export_txt,
+    DESCENDING as DESCENDING,
+    ASCENDING as ASCENDING,
+    TEXT as TEXT,
+    I as I,
+    D as D,
+    O as O,
+)
+from .importable import (
+    CSVImportable as CSVImportable,
+    JSONImportable as JSONImportable,
+    TXTImportable as TXTImportable,
+    Importable as Importable,
+    CSVImportableSelf,
+    JSONImportableSelf,
+    TXTImportableSelf,
+)
+from .utils import (
+    Countable as Countable,
+    epoch_now,
+    alive_bar_monitor,
+    is_alphanum,
+    get_url,
+    get_urls,
+    get_url_JSON_model,
+    get_url_JSON_models,
+    get_url_JSON,
+    get_urls_JSON,
+    get_urls_JSON_models,
+    get_datestr,
+    chunker,
+)
 
 
-__all__ = [ 'aliasmapper',
-			'asyncqueue',
-			'bucketmapper', 
-			'counterqueue', 
-			'eventcounter', 
-			'filequeue', 
-			'iterablequeue',
-			'multilevelformatter', 
-			'throttledclientsession',
-			'urlqueue', 
-			'utils'
-			]
+__all__ = [
+    "aliasmapper",
+    "asyncqueue",
+    "awrap",
+    "bucketmapper",
+    "counterqueue",
+    "eventcounter",
+    "filequeue",
+    "iterablequeue",
+    "multilevelformatter",
+    "throttledclientsession",
+    "urlqueue",
+    "utils",
+]
---
+++ src/pyutils/awrap.py
@@ -0,0 +1,24 @@
+"""AsyncWrap() is a async wrapper for Iterables
+
+It converts an Iterable[T] to AsyncIterable[T]
+"""
+
+from typing import AsyncIterable, AsyncIterator, Iterable, Iterator, TypeVar
+
+T = TypeVar("T")
+
+
+class AsyncWrapIterable(AsyncIterable[T]):
+    def __init__(self, iterable: Iterable[T]):
+        self.iterable: Iterable[T] = iterable
+        self.iter: Iterator[T]
+
+    def __aiter__(self) -> AsyncIterator[T]:
+        self.iter = iter(self.iterable)
+        return self
+
+    async def __anext__(self) -> T:
+        try:
+            return self.iter.__next__()
+        except StopIteration:
+            raise StopAsyncIteration
